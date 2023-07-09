076eb3cdd97ea2c97800fcae0724b086367b379e
gnaneswar-k <gnanu2004@gmail.com>
2023-07-09 23:28:16 +0530

    Updated backend link

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = axios.create({
   // baseURL: "http://127.0.0.1:3000", // Backend localhost. Replace with amazon aws link.
   // baseURL: "https://lepkbux71j.execute-api.ap-south-1.amazonaws.com/Prod", // Pranavasri VJS DynamoDB backend
-  baseURL: "https://c5nudd1i6e.execute-api.ap-south-2.amazonaws.com/Prod", // Gnaneswar Kulindala DynamoDB backend
+  baseURL: "https://yp4nx7c5kg.execute-api.ap-south-2.amazonaws.com/Prod", // Gnaneswar Kulindala DynamoDB backend
 });
 
 export default client;
